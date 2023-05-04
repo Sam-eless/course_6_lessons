48d2c6f28b02c397c0b17963411bb34d0cdaf588
Sam <iaiznura@gmail.com>
2023-05-04 21:13:56 +0300

    add models Category and Product

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.CharField(max_length=150, verbose_name='описание')
+    preview = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.CharField(max_length=150, verbose_name='категория')
+    purchase_price = models.IntegerField(verbose_name='цена за покупку')
+    date_of_creation = models.DateTimeField(verbose_name='дата создания')
+    last_modified_date = models.DateTimeField(verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name}, {self.description}, {self.preview}, {self.category},' \
+               f'{self.purchase_price}, {self.date_of_creation}, {self.last_modified_date}'
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('name',)
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.CharField(max_length=150, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}, {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('name',)

eff82b0ad87dd6fb9c3afe80af445c3cf8a5296f
Sam <iaiznura@gmail.com>
2023-05-15 22:19:04 +0300

    fix and rework command - fill

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -11,19 +11,26 @@ class Command(BaseCommand):
         Category.objects.all().delete()
         Product.objects.all().delete()
 
-        with open('catalog_data_2.json', 'r', encoding='UTF8') as file:
+        with open('catalog_data.json', 'r', encoding='UTF8') as file:
             data = json.load(file)
             category_objects = []
             product_objects = []
             for i in data:
                 if i['model'] == 'catalog.category':
-                    category_objects.append(Category(**i['fields']))
+                    category_objects.append(Category(i['pk'], **i['fields']))
+
+            Category.objects.bulk_create(category_objects)
+
+            for i in data:
                 if i['model'] == 'catalog.product':
                     name = Category.objects.get(pk=i["fields"]["category"])
                     i["fields"]["category"] = name
                     product_objects.append(Product(**i['fields']))
 
-        Category.objects.bulk_create(category_objects)
-        Product.objects.bulk_create(product_objects)
+            Product.objects.bulk_create(product_objects)
 
         # python -Xutf8 manage.py dumpdata catalog -o catalog_data.json
+        # python manage.py migrate catalog zero
+        # python manage.py makemigrations
+        # python manage.py migrate
+        # python manage.py fill
